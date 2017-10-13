ed71e8621ae2782a6b7cdc1ca8e76eb6badb32db
pantadeusz <none@none.com>
2017-10-13 14:57:32 +0200

    created simple example of Mockito

--- zao/02easymock/easyMockExample/src/test/java/pl/edu/puzniakowski/mocks/plotutils/PlotSquaresTest.java
+++ zao/02easymock/easyMockExample/src/test/java/pl/edu/puzniakowski/mocks/plotutils/PlotSquaresTest.java
@@ -1,13 +1,16 @@
 package pl.edu.puzniakowski.mocks.plotutils;
-//import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.junit.MockitoJUnitRunner;
 import pl.edu.puzniakowski.mocks.plotters.PlotterDriver;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PlotSquaresTest {
     PlotSquares plotSquares = new PlotSquares();
 
@@ -16,11 +19,24 @@ public class PlotSquaresTest {
 
     @Before
     public  void setup() {
+        assertNotNull(mockedPlotter);
         plotSquares.setPlotterDriver(mockedPlotter);
+        verify(mockedPlotter).resetPosition();
     }
 
     @Test
     public  void testDrawSquares() {
-        plotSquares.drawSquareInPlace(5);
+        int x0 = 10, y0 = 5, r = 7;
+        when(mockedPlotter.getPositionX()).thenReturn(x0);
+        when(mockedPlotter.getPositionY()).thenReturn(y0);
+        //when(mockedPlotter.gotoXY(x0 - r, y0 - r)).thenReturn(); -- we don't do that!!
+
+        plotSquares.drawSquareInPlace(r);
+        verify(mockedPlotter).getPositionX();
+        verify(mockedPlotter).getPositionY();
+        verify(mockedPlotter).gotoXY(x0 - r, y0 - r);
+        verify(mockedPlotter).gotoXY(x0 + r, y0 - r);
+        verify(mockedPlotter).gotoXY(x0 + r, y0 + r);
+        verify(mockedPlotter).gotoXY(x0 - r, y0 + r);
     }
 }
